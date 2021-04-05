41391dfbd630568722925861ae36e7cfe311f892
Vadim Savin <savinvadim1312@gmail.com>
2021-04-05 11:30:51 +0100

    connect with our wordpress

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `https://wpgatsbydemo.wpengine.com/graphql`,
+          `http://notjustdevelopmentblog.online/graphql`,
       },
     },
 
